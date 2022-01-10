9dc2a8d32ae57d9c0acd5ffd77b548ffec552253
Haris <amin.haris3323@gmail.com>
2022-01-10 07:18:35 +0700

    Additional Project and Skills

--- app.js
+++ app.js
@@ -1,22 +1,57 @@
-const projectCard=document.querySelector("#projectCard")
-const project=document.querySelector("#ProjectDetail")
-const btn1=document.querySelector("#btnradio1")
-const btn2=document.querySelector("#btnradio2")
-const services=document.querySelector("#ServiceContainer")
-const skills=document.querySelector("#SkillsContainer")
-projectCard.addEventListener("click",()=>{
-  videobackground.style.display = "inherit";
+$("#btnradio1").click(function(){
+  $("#SkillsContainer").css("display","none");
+  $("#ServiceContainer").css("display","flex")
 })
-btn1.addEventListener("click",()=>{
-  skills.style.display = "none";
-  services.style.display = "flex";
+$("#btnradio2").click(function(){
+  $("#SkillsContainer").css("display","flex");
+  $("#ServiceContainer").css("display","none")
 })
-btn2.addEventListener("click",()=>{
-  skills.style.display = "flex";
-  services.style.display = "none";
+$("#preview").click(function(){
+  $("#videobackground").css("display","inherit")
+})
+$("#device1").click(function(){
+  $("#videobackground").css("display","inherit")
+})
+$("#device2").click(function(){
+  $("#videobackground").css("display","inherit")
+})
+$("#videobackground").click(function(){
+  $("#videobackground").css("display","none")
+})
+$("#close").click(function(){
+  $("#videobackground").css("display","none")
 })
 
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+// window.onbeforeunload = function () {
+//   window.scrollTo(0, 0);
+// };
+
+// Bootstrap View Description
+var popoverTriggerList = [].slice.call($('[data-bs-toggle="popover"]'))
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
+
+// GSAP Nav Icon Scroll Animation
+let navscroll = gsap.timeline({
+  scrollTrigger:{
+      trigger:'#navbars',
+      start:"center top",
+      end:"center -4000px",
+      pin:"#navicon",
+      toggleActions:"restart none none reverse",
+  }
+});
+let hamburgericon = gsap.timeline({
+  scrollTrigger:{
+    trigger:'#mobilebtn',
+    start:"top top",
+    end:"bottom -500%",
+    pin:"#hamburger",
+  }
+});
+
+navscroll.to("#navP",{x:"0px",y:"600px",duration:0.1})
+.to("#navQ",{x:"100px",y:"500px",duration:0.1})
+.to("#navabout",{x:"200px",y:"400px",duration:0.1})
+.to("#navhome",{x:'300px',y:"300px",duration:0.1})
